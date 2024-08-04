b7cd691004c5a3c4401bc057bdade6dbe3970a86
Dev Govindji <dev.govindji.software@gmail.com>
2024-08-04 14:55:19 -0500

    adjusts panels view per viewport

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ import ItemDetails from './ItemDetails';
 import styles from './styles.module.css';
 
 const pages = [
-  // '/videos/11952337_1440_2560_30fps.mp4',
+  '/videos/11952337_1440_2560_30fps.mp4',
+  '/videos/openSandwiches.mp4',
   '/videos/friedEggs.mp4',
   '/videos/stirFry.mp4',
-  '/videos/openSandwiches.mp4',
   'https://images.pexels.com/photos/62689/pexels-photo-62689.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
   'https://images.pexels.com/photos/4016596/pexels-photo-4016596.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
   'https://images.pexels.com/photos/351265/pexels-photo-351265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -52,17 +52,19 @@ const MediaComponent = ({ url }: { url: string }) => {
 
   if (mediaType === 'image') {
     return (
-      <animated.div style={{ width: '100vw', height: '100vh', backgroundImage: `url(${url})`, backgroundSize: 'cover', backgroundPosition: 'center' }} />
+      <animated.div style={{ width: '120vw', height: '120vh', backgroundImage: `url(${url})`, backgroundSize: 'cover', backgroundPosition: 'bottom 1% right 1%', aspectRatio: "16/9", position: "absolute", bottom: "2vh", right: "85%" }} />
     );
   } else if (mediaType === 'video') {
     return (
-      <animated.video ref={videoRef} style={{ width: '100%', height: '100%' }} muted autoPlay loop>
+      <animated.video ref={videoRef} style={{ width: '120rm', objectFit: 'cover', objectPosition: 'bottom 1% right 1%', aspectRatio: "16/9", position: "absolute", bottom: "5vh", right: "85%" }} muted autoPlay loop>
         <source src={url} type="video/mp4" />
         Your browser does not support the video tag.
       </animated.video>
     );
   }
 
+  // objectFit: 'cover', objectPosition: 'center',
+  // backgroundSize: 'cover', backgroundPosition: 'center',
   return null;
 };
 
@@ -88,6 +90,10 @@ function Viewpager() {
       if (i < index.current - 1 || i > index.current + 1) return { display: 'none' };
       const x = (i - index.current) * width + (active ? mx : 0);
       const scale = active ? 1 - distance / width / 2 : 1;
+      console.log('x: ', x)
+      console.log('width: ', width)
+      console.log('distance: ', distance)
+      console.log('scale: ', scale)
       return { x, scale, display: 'block' };
     });
   });
@@ -96,6 +102,8 @@ function Viewpager() {
   const openDetails = (event: any) => {
     navigate(`/item/01`);
   };
+
+  console.log('styles.wrapper: ', styles.wrapper)
 
   return (
     <div ref={ref} className={styles.wrapper}>
