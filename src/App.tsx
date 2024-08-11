e1b792a509e0dd4c253720a7df93bac90a7ec97d
Dev Govindji <dev.govindji.software@gmail.com>
2024-08-10 23:00:28 -0500

    Adds labels specific to each panel

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,29 @@ const pages = [
   '/videos/porkchops.mp4',
   '/videos/bbqRibs.mp4',
   '/videos/chicken.mp4',
-  '/videos/11952337_1440_2560_30fps.mp4',
+  '/videos/shakshouka.mp4',
   '/videos/openSandwiches.mp4',
   '/videos/friedEggs.mp4',
   '/videos/stirFry.mp4',
-  'https://images.pexels.com/photos/62689/pexels-photo-62689.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-  'https://images.pexels.com/photos/4016596/pexels-photo-4016596.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-  'https://images.pexels.com/photos/351265/pexels-photo-351265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-  'https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  // 'https://images.pexels.com/photos/62689/pexels-photo-62689.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  // 'https://images.pexels.com/photos/4016596/pexels-photo-4016596.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  // 'https://images.pexels.com/photos/351265/pexels-photo-351265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  // 'https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  // 'https://images.pexels.com/photos/296878/pexels-photo-296878.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+]
+const titles = [
+  'Bacon',
+  'Pork Chops',
+  'BBQ Ribs',
+  'Chicken',
+  'Shakshouka',
+  'Open Sandwiches',
+  'Fried Eggs',
+  'Stir Fry',
+  // 'https://images.pexels.com/photos/62689/pexels-photo-62689.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  // 'https://images.pexels.com/photos/4016596/pexels-photo-4016596.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  // 'https://images.pexels.com/photos/351265/pexels-photo-351265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  // 'https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
   // 'https://images.pexels.com/photos/296878/pexels-photo-296878.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
 ]
 
@@ -28,7 +43,6 @@ const pages = [
 
 const getMediaType = (url: string) => {
   const extension = url.split('.').pop()?.toLowerCase();
-  console.log('extension: ', extension);
   if (extension === 'mp4') {
     return 'video';
   } else if (['jpeg', 'jpg', 'png', 'gif'].includes(extension!)) {
@@ -37,7 +51,8 @@ const getMediaType = (url: string) => {
   return 'image';
 };
 
-const MediaComponent = ({ url }: { url: string }) => {
+const MediaComponent = ({idx}: {idx:any} ) => {
+  const url: string = pages[idx]
   const mediaType = getMediaType(url);
   const videoRef = useRef<HTMLVideoElement>(null);
 
@@ -59,13 +74,13 @@ const MediaComponent = ({ url }: { url: string }) => {
   if (mediaType === 'image') {
     return (
       <animated.span style={{ width: '100vw', height: '100vh', backgroundImage: `url(${url})`, backgroundSize: 'cover', backgroundPosition: 'bottom 1% right 1%', aspectRatio: "16/9", position: "fixed" }} >
-        <p style={{ fontSize: '15vw', color: 'rgba(255, 255, 255, 0.5)', width: '100%', height: '30%', backgroundColor: 'rgba(50, 50, 73, 0.5)', aspectRatio: "16/9" }}>Chicken</p>
+        <p style={{ fontSize: '15vw', color: 'rgba(255, 255, 255, 0.5)', width: '100%', height: '30%', backgroundColor: 'rgba(50, 50, 73, 0.5)', aspectRatio: "16/9" }}>{titles[idx]}</p>
       </animated.span>
     );
   } else if (mediaType === 'video') {
     return (
       <animated.span style={{ width: '100vw', height: '100vh', aspectRatio: "16/9" }} >
-        <p style={{ zIndex: '100', fontSize: '15vw', color: 'rgba(255, 255, 255, 0.5)', width: '100%', height: '30%', backgroundColor: 'rgba(50, 50, 73, 0.5)', aspectRatio: "16/9" }}>Pork Chops</p>
+        <p style={{ zIndex: '100', fontSize: '15vw', color: 'rgba(255, 255, 255, 0.5)', width: '100%', height: '30%', backgroundColor: 'rgba(50, 50, 73, 0.5)', aspectRatio: "16/9" }}>{titles[idx]}</p>
         <video ref={videoRef} style={{ zIndex: '-1', width: '100vw', objectFit: 'cover', objectPosition: 'bottom 1% right 1%', aspectRatio: "16/9", position: "absolute", bottom: "25%" }} muted autoPlay loop>
           <source src={url} type="video/mp4" />
           Your browser does not support the video tag.
@@ -101,10 +116,6 @@ function Viewpager() {
       if (i < index.current - 1 || i > index.current + 1) return { display: 'none' };
       const x = (i - index.current) * width + (active ? mx : 0);
       const scale = active ? 1 - distance / width / 2 : 1;
-      console.log('x: ', x)
-      console.log('width: ', width)
-      console.log('distance: ', distance)
-      console.log('scale: ', scale)
       return { x, scale, display: 'block' };
     });
   });
@@ -114,14 +125,12 @@ function Viewpager() {
     navigate(`/item/01`);
   };
 
-  console.log('styles.wrapper: ', styles.wrapper)
-
   return (
     <div ref={ref} className={styles.wrapper}>
       {props.map(({ x, display, scale }, i) => (
         <animated.div className={styles.page} {...bind()} key={i} style={{ display, x }}>
           <animated.div style={{ scale }} onDoubleClick={openDetails}>
-            <MediaComponent url={pages[i]} />
+            <MediaComponent idx={i} />
           </animated.div>
         </animated.div>
       ))}
